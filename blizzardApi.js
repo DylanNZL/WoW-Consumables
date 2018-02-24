921f175a9da025787ddaebe990b424abe3b72667
Dylan Cross <dylancross@me.com>
2018-02-24 20:38:18 +1300

    Inserting auction house data with history table reference

--- blizzardApi.js
+++ blizzardApi.js
@@ -4,7 +4,7 @@
  * This can take a long time because there is a lot of auctions (Frostmourne has roughly 90k)
  *
  * TODO:
- *      Refresh the auction house data every so often (~2 hours):
+ *      Refresh the auction house data every so often (~12 hours):
  *      Need to remove auctions that weren't updated in the update (timeLeft: short for longer than short is equal to)
  */
 
@@ -13,6 +13,7 @@ const API_KEY = require("./api_key.js");
 const wowServer = "frostmourne"; // Change to any NA server
 // const wowServer = "amanthul"; // lowest pop oce realm for testing
 const queue = require("./queue.js");
+const database = require("./database.js");
 
 const prequest = require('prequest');
 
@@ -47,7 +48,7 @@ async function callBlizzardAH() {
                     } else {
                         const length = dat.auctions.length;
                         console.log(Date.now() + " Amount of auctions: " + length);
-                        saveAHData(dat.auctions);
+                        saveAHData(dat.auctions, length);
                         resolve (1);
                     }
                 }).catch(function (err) {
@@ -65,10 +66,13 @@ async function callBlizzardAH() {
 }
 // {"auc":1591238360,"item":124437,"owner":"NAME","ownerRealm":"Frostmourne","bid":6650000,"buyout":7000000,"quantity":200,"timeLeft":"LONG","rand":0,"seed":0,"context":0},
 
-function saveAHData(data) {
+async function saveAHData(data, length) {
+    let history = await database.newApiCall(length);
+    console.log("downloaded AH data, saving as history " + history);
     data.forEach(function (d) {
         // console.log(d.auc);
         let obj = {
+            history: history,
             auc: d.auc,
             item: d.item,
             owner: d.owner,
@@ -81,57 +85,5 @@ function saveAHData(data) {
         queue.q.push(obj);
     });
 }
-
-const itemIds = [
-    133579,
-    133565,
-    124119,
-    124120,
-    133566,
-    124117,
-    124111,
-    133567,
-    124112,
-    133568,
-    124110,
-    133569,
-    124109,
-    133592,
-    133680,
-    133578,
-    133557,
-    133561,
-    124108,
-    133562,
-    133563,
-    124121,
-    133564,
-];
-
-const itemsArray = [
-    { "id" : 133579, "name" : "Lavish Suramar Feast" },
-    { "id" : 133565, "name" : "Leybeque Ribs" },
-    { "id" : 124119, "name" : "Big Gamy Ribs" },
-    { "id" : 124120, "name" : "Leyblood" },
-    { "id" : 133566, "name" : "Suramar Surf and Turf" },
-    { "id" : 124117, "name" : "Lean Shank" },
-    { "id" : 124111, "name" : "Runescale Koi" },
-    { "id" : 133567, "name" : "Barracude Mrglgagh" },
-    { "id" : 124112, "name" : "Black Barracuda" },
-    { "id" : 133568, "name" : "Koi-Scented Stormray" },
-    { "id" : 124110, "name" : "Stormray" },
-    { "id" : 133569, "name" : "Drogbar Style Salmon" },
-    { "id" : 124109, "name" : "Highmountain Salmon" },
-    { "id" : 133592, "name" : "Stonedark Snail" },
-    { "id" : 133680, "name" : "Slice of Bacon" },
-    { "id" : 133578, "name" : "Hearty Feast" },
-    { "id" : 133557, "name" : "Salt and Pepper Shank" },
-    { "id" : 133561, "name" : "Deep-Fried Mossgill" },
-    { "id" : 124108, "name" : "Mossgill Perch" },
-    { "id" : 133562, "name" : "Pickled Stormray" },
-    { "id" : 133563, "name" : "Faronaar Fizz" },
-    { "id" : 124121, "name" : "Wildfowl Egg" },
-    { "id" : 133564, "name" : "Spiced Rib Roast" }
-];
 
 exports.getAHData = getAHData;
