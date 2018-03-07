3960c4d1c1d209c36b04d518e69d725cc18a9747
Dylan Cross <dylancross@me.com>
2018-03-07 19:48:10 +1300

    Updated craftables to use the included craftables when used in another craftable (feast, cauldron, etc)

--- routes/api.js
+++ routes/api.js
@@ -324,12 +324,13 @@ async function allReagents(req,res,next) {
 
 /**
  * NOTE: item ids containing their actual 6 letter ids are found in the /api/allReagents
+ *       item ids starting with 8xxxxxx are found in this api call (375 stat food uses 1 of the 300 stat food in crafting it)
  *       item ids starting with 9xxxxxx are found in the /api/shopReagents
  */
 const craftables = {
     // Flasks
-    127851: { id : 127851, name : "Spirit Cauldron", category : "flask", recipe: [{ id : 127847, quantity : 5 },
-            { id : 127848, quantity : 5 },{ id : 127849, quantity : 5 },{ id : 127850, quantity : 5 }] },
+    127851: { id : 127851, name : "Spirit Cauldron", category : "flask", recipe: [{ id : 8127847, quantity : 5 },
+            { id : 8127848, quantity : 5 },{ id : 8127849, quantity : 5 },{ id : 8127850, quantity : 5 }] },
     127847: { id : 127847, name : "Flask of the Whispered Pact", category : "flask", recipe: [
         { id: 124104, quantity: 10 }, { id: 124102, quantity: 10 }, { id: 124105, quantity: 7 }] }, // Fjarnskaggl, Dreamleaf
     127848: { id : 127848, name : "Flask of the Seventh Demon", category : "flask", recipe: [
@@ -347,28 +348,28 @@ const craftables = {
             { id: 9133588, quantity: 2 }, { id: 9133589, quantity: 2 }] },
     133565: { id : 133565, name : "Leybeque Ribs", category : "Critical Strike", stat: 300, recipe: [{ id: 124120, quantity: 5 },
             { id: 124119, quantity: 5 }, { id: 9133589, quantity: 5 }] },
-    133570: { id : 133570, name : "The Hungry Magister", category : "Critical Strike", stat: 375, recipe: [{ id: 133565, quantity: 1 },
+    133570: { id : 133570, name : "The Hungry Magister", category : "Critical Strike", stat: 375, recipe: [{ id: 8133565, quantity: 1 },
             { id: 124109, quantity: 5 }, { id: 124118, quantity: 5 }, { id: 9133588, quantity: 3 }, { id: 9133589, quantity: 2 }] },
     // Haste
     133561: { id : 133561, name : "Deep-Fried Mossgill", category : "Haste", stat: 225, recipe: [{ id: 124108, quantity: 5 },
             { id: 9133590, quantity: 5 }] },
     133566: { id : 133566, name : "Suramar Surf and Turf", category : "Haste", stat: 300, recipe: [{ id: 124111, quantity: 5 },
             { id: 124117, quantity: 2 }, { id: 9133588, quantity: 2 }, { id: 9133593, quantity: 4 }] },
-    133571: { id : 133571, name : "Azshari Salad", category : "Haste", stat: 375, recipe: [{ id: 133571, quantity: 1 },
+    133571: { id : 133571, name : "Azshari Salad", category : "Haste", stat: 375, recipe: [{ id: 8133571, quantity: 1 },
             { id: 124101, quantity: 5 }, { id: 124102, quantity: 5 }, { id: 124103, quantity: 5 }, { id: 124104, quantity: 5 }] },
     // Vers
     133563: { id : 133563, name : "Faronaar Fizz", category : "Versatility", stat: 225, recipe: [{ id: 124121, quantity: 5 },
             { id: 9133591, quantity: 2 }, { id: 9133593, quantity: 3 }] },
     133568: { id : 133568, name : "Koi-Scented Stormray", category : "Versatility", stat: 300, recipe: [{ id: 124111, quantity: 5 },
             { id: 124110, quantity: 5 }, { id: 9133588, quantity: 2 }, { id: 9133590, quantity: 2 }] },
-    133573: { id : 133573, name : "Seed-Battered Fish Plate", category : "Versatility", stat: 375, recipe: [{ id: 133568, quantity: 1 },
+    133573: { id : 133573, name : "Seed-Battered Fish Plate", category : "Versatility", stat: 375, recipe: [{ id: 8133568, quantity: 1 },
             { id: 133607, quantity: 5 }, { id: 128304, quantity: 20 }, { id: 9133590, quantity: 3 }, { id: 9133589, quantity: 2 }] },
     // Mastery
     133562: { id : 133562, name : "Pickled Stormray", category : "Mastery", stat: 275, recipe: [{ id: 124110, quantity: 5 },
             { id: 9133588, quantity: 3 }] },
     133567: { id : 133567, name : "Barracuda Mrglgagh", category : "Mastery", stat: 300, recipe: [{ id: 124120, quantity: 5 },
             { id: 124112, quantity: 5 }, { id: 9133590, quantity: 4 }, { id: 9133591, quantity: 1 }] },
-    133572: { id : 133572, name : "Nightborne Delicacy Platter", category : "Mastery", stat: 375, recipe: [{ id: 133567, quantity: 1 },
+    133572: { id : 133572, name : "Nightborne Delicacy Platter", category : "Mastery", stat: 375, recipe: [{ id: 8133567, quantity: 1 },
             { id: 129100, quantity: 20 }, { id: 124105, quantity: 1 }, { id: 124121, quantity: 5 }, { id: 9133592, quantity: 10 }] },
     // Misc food // TODO: More misc food?
     133564: { id : 133564, name : "Spiced Rib Roast", category : "MISC", stat: 0, recipe: [{ id: 124119, quantity: 5 },
@@ -376,11 +377,11 @@ const craftables = {
     133569: { id : 133569, name : "Drogbar Style Salmon", category : "MISC", stat: 0, recipe: [{ id: 124109, quantity: 5 },
             { id: 9133592, quantity: 10 }, { id: 9133589, quantity: 4 }, { id: 9133593, quantity: 10 }] },
     // Feasts
-    133578: { id : 133578, name : "Hearty Feast", category : "Feast", stat: 400, recipe: [{ id: 133557, quantity: 6 },
-            { id: 133561, quantity: 6 },{ id: 133563, quantity: 6 },{ id: 133562, quantity: 6 },{ id: 133564, quantity: 6 },
+    133578: { id : 133578, name : "Hearty Feast", category : "Feast", stat: 400, recipe: [{ id: 8133557, quantity: 6 },
+            { id: 8133561, quantity: 6 },{ id: 8133563, quantity: 6 },{ id: 8133562, quantity: 6 },{ id: 133564, quantity: 6 },
             { id: 133680, quantity: 6}] }, // NOTE: Using 6 so you can minus the current rank to get the actual quantity
-    133579: { id : 133579, name : "Lavish Suramar Feast" , category : "Feast", stat: 500, recipe: [{ id: 133565, quantity: 6 },
-            { id: 133566, quantity: 6 },{ id: 133568, quantity: 6 },{ id: 133567, quantity: 6 },{ id: 133569, quantity: 6 },
+    133579: { id : 133579, name : "Lavish Suramar Feast" , category : "Feast", stat: 500, recipe: [{ id: 8133565, quantity: 6 },
+            { id: 8133566, quantity: 6 },{ id: 8133568, quantity: 6 },{ id: 8133567, quantity: 6 },{ id: 8133569, quantity: 6 },
             { id: 133680, quantity: 6}] }, // NOTE: Using 6 so you can minus the current rank to get the actual quantity
 };
 async function allCraftables(req, res, next) {
@@ -410,21 +411,18 @@ async function allCraftables(req, res, next) {
     res.status(200).jsonp(result);
 }
 
-
-const shopReagents = {
-    9133588 : { id : 133588, name : "Flaked Sea Salt", cost : 0.5 },
-    9133589 : { id : 133589, name : "Dalapeño Pepper", cost : 0.5 },
-    9133590 : { id : 133590, name : "Muskenbutter", cost : 0.5 },
-    9133591 : { id : 133591, name : "River Onion", cost : 0.5 },
-    9133592 : { id : 133592, name : "Stonedark Snail", cost : 0.5 },
-    9133593 : { id : 133593, name : "Royal Olive", cost : 0.5 }
-};
-
 async function shopReagents(req, res, next) {
     let result = {
         timestamp: Date.now(),
         success: true,
-        items: shopReagents
+        items: {
+            9133588 : { id : 133588, name : "Flaked Sea Salt", cost : 0.5 },
+            9133589 : { id : 133589, name : "Dalapeño Pepper", cost : 0.5 },
+            9133590 : { id : 133590, name : "Muskenbutter", cost : 0.5 },
+            9133591 : { id : 133591, name : "River Onion", cost : 0.5 },
+            9133592 : { id : 133592, name : "Stonedark Snail", cost : 0.5 },
+            9133593 : { id : 133593, name : "Royal Olive", cost : 0.5 }
+        }
     };
 
     res.status(200).jsonp(result);
