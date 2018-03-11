41d58b0e132d90d4fa4638699a759f1d43eddabf
Dylan Cross <dylancross@me.com>
2018-03-11 19:27:09 +1300

    Flasks and potions are now able to be displayed in the test page

--- public/js/test.js
+++ public/js/test.js
@@ -1,3 +1,120 @@
+
+
+// Works for Food, Flasks and Potions, however feast require a bit more detail so have their own template
+Vue.component('c-template', {
+    template: "#card-template",
+    props: ["info", "items"],
+    methods: {
+        rankChanged: function() {
+        },
+        rankModifier: function (cost, mInfo) {
+            if (mInfo.category === "Mastery" || mInfo.category === "Haste" || mInfo.category === "Versatility" || mInfo.category === "Critical Strike") {
+                if (mInfo.rank.selected === 3) { return cost / 10; }
+                if (mInfo.rank.selected === 2) { return cost / 7; }
+                else { return cost / 5; }
+            } else if (mInfo.category === "flask") {
+                if (mInfo.rank.selected === 3) { return cost / 1.4; } // I think it works out between 1.3 and 1.5 flasks created per flask made at rank 3
+                else { return cost; }
+            } else if (mInfo.category === "potion") {
+                if (mInfo.rank.selected === 3) { return cost / 1.2; } // Each pot seems to have different proc rates so conservative estimate
+                else { return cost; }
+            }
+            return cost;
+        },
+        cost: function(mInfo, mItems) {
+            let cost = 0.0;
+            if (mItems.allReagents[124101] === undefined) return 0.0;
+
+            mInfo.recipe.forEach(function(data) {
+                if (data.id < 8000000) {
+                    if (mItems.allReagents[data.id].buyoutData === undefined) return 0.0;
+                    cost += mItems.allReagents[data.id].buyoutData.average * data.quantity;
+                } else if (data.id < 9000000) {
+                    let id = data.id % 8000000;
+                    if (mItems.craftables[id].buyoutData === undefined) return 0.0;
+                    cost += mItems.craftables[id].buyoutData.average * data.quantity;
+                } else {
+                    let id = data.id % 9000000;
+                    if (mItems.shopReagents[id].buyoutData === undefined) return 0.0;
+                    cost += mItems.shopReagents[id].buyoutData.average * data.quantity;
+                }
+
+            });
+
+            cost = this.rankModifier(cost, mInfo);
+            return cost.toFixed(2);
+        },
+        minCost: function(mInfo, mItems) {
+            let cost = 0.0;
+            if (mItems.allReagents[124101] === undefined) return 0.0;
+
+            mInfo.recipe.forEach(function(data) {
+                if (data.id < 8000000) {
+                    if (mItems.allReagents[data.id].buyoutData === undefined) return 0.0;
+                    cost += mItems.allReagents[data.id].buyoutData.min * data.quantity;
+                } else if (data.id < 9000000) {
+                    let id = data.id % 8000000;
+                    if (mItems.craftables[id].buyoutData === undefined) return 0.0;
+                    cost += mItems.craftables[id].buyoutData.average * data.quantity;
+                } else {
+                    let id = data.id % 9000000;
+                    if (mItems.shopReagents[id].buyoutData === undefined) return 0.0;
+                    cost += mItems.shopReagents[id].buyoutData.average * data.quantity;
+                }
+
+            });
+
+            cost = this.rankModifier(cost, mInfo);
+            return cost.toFixed(2);
+        },
+    }
+});
+
+Vue.component('tr-template', {
+    template: "#tableRow-template",
+    props: ["item", "items"],
+    methods: {
+        name: function (mItem, mItems) {
+            if (mItem.id < 8000000) {
+                return mItems.allReagents[mItem.id].name;
+            } else if (mItem.id < 9000000) {
+                return mItems.craftables[mItem.id % 8000000].name;
+            } else {
+                return mItems.craftables[mItem.id % 9000000].name;
+            }
+        },
+        average: function (mItem, mItems) {
+            if (mItem.id < 8000000) {
+                return mItems.allReagents[mItem.id].buyoutData.average;
+            } else if (mItem.id < 9000000) {
+                return mItems.craftables[mItem.id % 8000000].buyoutData.average;
+            } else {
+                return mItems.craftables[mItem.id % 9000000].buyoutData.average;
+            }
+        },
+        min: function (mItem, mItems) {
+            if (mItem.id < 8000000) {
+                return mItems.allReagents[mItem.id].buyoutData.min;
+            } else if (mItem.id < 9000000) {
+                return mItems.craftables[mItem.id % 8000000].buyoutData.min;
+            } else {
+                return mItems.craftables[mItem.id % 9000000].buyoutData.min;
+            }
+        },
+        max: function (mItem, mItems) {
+            if (mItem.id < 8000000) {
+                return mItems.allReagents[mItem.id].buyoutData.max;
+            } else if (mItem.id < 9000000) {
+                return mItems.craftables[mItem.id % 8000000].buyoutData.max;
+            } else {
+                return mItems.craftables[mItem.id % 9000000].buyoutData.max;
+            }
+        },
+        count: function (item) {
+        },
+    }
+});
+
 new Vue({
     el: '#app',
     data: {
@@ -31,7 +148,7 @@ new Vue({
                 // console.log(data);
                 vthis.items.allReagents = data.items;
                 for (let item in vthis.items.allReagents) {
-                    console.log(item);
+                    // console.log(item);
                     vthis.items.allReagents[item].buyoutData = buyoutData(vthis.items.allReagents[item].auctions);
                 }
             });
@@ -47,7 +164,7 @@ new Vue({
                 vthis.items.craftables = data.items;
 
                 for (let item in vthis.items.craftables) {
-                    console.log(item);
+                    // console.log(item);
                     vthis.items.craftables[item].buyoutData = buyoutData(vthis.items.craftables[item].auctions);
                 }
             });
@@ -92,7 +209,7 @@ new Vue({
  * I only include auctions that include 20 or more items as smaller quantities will skew the price lower than what is correct for crafting large numbers
  */
 function buyoutData(auctions) {
-    console.log(auctions);
+    // console.log(auctions);
     if (auctions.length === 0) {
         return {
             average : undefined,
